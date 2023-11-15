8a24c043fe5dab52fdb8fa921544b3f0761e4404
Thomas Abbink <thomas.abbink@infi.nl>
2023-11-15 15:29:10 +0100

    added a new game button

--- src/components/SudokuBoard.vue
+++ src/components/SudokuBoard.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { useSudoku, cellValues } from '@/stores/sudoku'
-import type { CellValue, Cell } from '@/stores/sudoku'
-import { ref, watch } from 'vue'
+import { useSudoku } from '@/stores/sudoku'
+import type {} from '@/stores/sudoku'
+import { watch } from 'vue'
+import Button from '../components/Button.vue'
+import { cellValues, type Cell, type CellValue } from '@/model/Sudoku'
 
 const { grid, $reset } = useSudoku()
-$reset()
 
 watch(
   grid.cells,
@@ -14,13 +15,6 @@ watch(
   },
   { deep: true }
 )
-
-const itemRefs = ref<HTMLInputElement[]>([])
-// umm wtf Vue. give me access to the input in an event handler. I don't
-// want to keep a ref for all these inputs
-const selectAll = (id: number) => {
-  itemRefs.value[id].select()
-}
 
 const parseInput = (input: string): Cell['enteredValue'] => {
   if (!input) {
@@ -46,33 +40,33 @@ const handleInput = (event: Event, cell: Cell) => {
 </script>
 
 <template>
-  <ol
-    class="grid grid-cols-[repeat(9,2.5rem)] grid-rows-[repeat(9,2.5rem)] w-fit place-self-center"
-  >
-    <li
-      v-for="cell in grid.cells"
-      :key="cell.index"
-      class="[&:nth-child(3n+0)]:border-r-4 [&:nth-child(9n)]:border-r-0 border-slate-800 [&:nth-child(n+19)]:border-b-4 [&:nth-child(n+28)]:!border-b-0 [&:nth-child(n+46)]:!border-b-4 [&:nth-child(n+55)]:!border-b-0 select-none"
+  <div class="flex flex-col gap-6">
+    <ol
+      v-if="grid.cells"
+      class="grid grid-cols-[repeat(9,2.5rem)] grid-rows-[repeat(9,2.5rem)] w-fit place-self-center"
     >
-      <input
-        :title="cell.possibleValues.toString()"
-        @focus="(_) => selectAll(cell.index)"
-        ref="itemRefs"
-        min="1"
-        max="9"
-        type="number"
-        :value="cell.isInitial ? cell.value : cell.enteredValue"
-        @input="(event) => handleInput(event, cell)"
-        maxlength="1"
-        :disabled="cell.isInitial"
-        class="text-slate-950 w-full h-full flex items-center justify-center text-center outline-none font-bold text-xl caret-transparent [input::-webkit-outer-spin-button] [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none border border-slate-500 selection:bg-transparent transition-colors duration-75"
-        :class="{
-          'bg-slate-300': cell.isInitial,
-          'bg-white hover:bg-sky-100 cursor-pointer focus-visible:bg-sky-200': !cell.isInitial,
-          'border-green-500': cell.enteredValue === cell.value,
-          'border-red-400': cell.enteredValue && cell.enteredValue !== cell.value
-        }"
-      />
-    </li>
-  </ol>
+      <li
+        v-for="cell in grid.cells"
+        :key="cell.index"
+        class="[&:nth-child(3n+0)]:border-r-4 [&:nth-child(9n)]:border-r-0 border-slate-800 [&:nth-child(n+19)]:border-b-4 [&:nth-child(n+28)]:!border-b-0 [&:nth-child(n+46)]:!border-b-4 [&:nth-child(n+55)]:!border-b-0 select-none"
+      >
+        <input
+          ref="itemRefs"
+          min="1"
+          max="9"
+          type="number"
+          :value="cell.isRevealed ? cell.value : cell.enteredValue"
+          @input="(event) => handleInput(event, cell)"
+          maxlength="1"
+          :disabled="cell.isRevealed"
+          class="text-slate-950 w-full h-full flex items-center justify-center text-center outline-none font-bold text-xl caret-transparent [input::-webkit-outer-spin-button] [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none border border-slate-500 selection:bg-transparent transition-colors duration-75"
+          :class="{
+            'bg-slate-300': cell.isRevealed,
+            'bg-white hover:bg-sky-100 cursor-pointer focus-visible:bg-sky-200': !cell.isRevealed
+          }"
+        />
+      </li>
+    </ol>
+    <Button @click="$reset"> New game </Button>
+  </div>
 </template>
