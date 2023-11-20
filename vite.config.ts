4b6f44f638123e282c1b6701c89144ded465b8fd
Thomas Abbink <thomas.abbink@infi.nl>
2023-11-20 16:35:46 +0100

    added vite pwa capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,35 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', '/img/icons/apple-touch-icon.png'],
+      manifest: {
+        name: 'Vuedoku',
+        short_name: 'Vuedoku',
+        description: 'vue a sudoku',
+        theme_color: '#f87171',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
